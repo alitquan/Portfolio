11ed6c635d7379e97fb4f5a5b53e1ed65e6fa29d
alitquan <alitmallick.gmail.com>
2024-07-24 17:03:47 -0400

    Smooth scroll has been installed

--- vite-portfolio/src/sections/Home.jsx
+++ vite-portfolio/src/sections/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './HomeStyles.module.css' 
 import hollywood from '../assets/hollywood6.png'
+import { Link, animateScroll as scroll} from 'react-scroll'
 function Home() {
 	return (
 		<section id="home" className={styles.container}>
@@ -13,8 +14,10 @@ function Home() {
 				<p> DevOps Engineer </p>		
 			</div> 
 
-			<div className={styles.containerNavigation}> 
-				<p> ABOUT ME </p>
+			<div className={styles.containerNavigation}>
+				<nav>
+					<Link activeClass="active" to="about-me" spy={true} smooth={true} duration={500}> <p>About Me</p> </Link>	
+				</nav> 
 				<p> RESUME </p> 
 				<p> PROJECTS </p>
 				<p> CONTACT ME </p>		
