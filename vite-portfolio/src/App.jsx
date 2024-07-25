f2e4f3bbadfcca6e3bdba1a739730230d9215f83
alitquan <alitmallick.gmail.com>
2024-07-25 08:44:18 -0400

    Added template for side projects

--- vite-portfolio/src/App.jsx
+++ vite-portfolio/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Home from './sections/Home.jsx'; 
 import AboutMe from './sections/AboutMe.jsx';
+import SideProjects from './sections/SideProjects.jsx';
+
 
 function App() {
   return <>
 	<Home /> 
 	<AboutMe />;
+	<SideProjects />;
 	</>;
 }
+ 
 
 export default App;
--- vite-portfolio/src/sections/SideProjects.jsx
+++ vite-portfolio/src/sections/SideProjects.jsx
@@ -4,7 +4,30 @@ import styles from './HomeStyles.module.css'
 
 function SideProjects() {
 	return ( 
-		<section id="sideprojects" className={style.container}>
+		<section id="sideprojects" className={styles.container}>
+			<div className={styles2.gridProjects}>
+				<div className = {styles2.gridProjectsRow}> 
+					<div className = {styles2.gridProjectsItem}>
+						One
+					</div>
+
+					<div className = {styles2.gridProjectsItem}>
+						Two
+					</div>
+
+				</div> 
+				<div className = {styles2.gridProjectsRow}> 
+					<div className = {styles2.gridProjectsItem}>
+						Three
+					</div>
+
+					<div className = {styles2.gridProjectsItem}>
+						Four
+					</div>
+
+				</div> 
+			</div> 
+
 
 		</section>
 	);
